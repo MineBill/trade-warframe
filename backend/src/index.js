5484603bcbbb549a13f1f6689773ee93a4c0e277
MatinaC553 <118002786+MatinaC553@users.noreply.github.com>
2022-12-14 20:21:07 +0200

    database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
-
+const { Sequelize } = require('sequelize');
 dotenv.config();
-app.use(cors());
+const sequelize = new Sequelize(`mariadb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:3306/${process.env.DB_NAME}`)
 
-app.get("/", (req, res) => {
-    res.send("Hello, World");
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+    setup();
+}).catch(error => {
+    console.error('Unable to connect to the database:', error);
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
-});
+function setup() {
+    app.use(cors());
+
+    app.get("/", (req, res) => {
+        res.send("Hello, World");
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Listening on port ${process.env.PORT}`)
+    });
+}
