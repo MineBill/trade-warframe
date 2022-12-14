c469435f547c08aaec184d100b0e16839eeea408
MineBill <minebill@pm.me>
2022-12-14 17:56:00 +0200

    Use cors to bypass resource errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-let counter = 0
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello, World");
-});
-
-app.get("/counter", (req, res) => {
-    res.send(`Counter is: ${counter++}`);
 });
 
 app.listen(port, () => {
