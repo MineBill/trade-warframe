1773363a0b4ef5e660a40748c1d6d0cc687d4e85
cs161146 <118004634+cs161146@users.noreply.github.com>
2022-12-24 16:37:18 +0200

    3.3

--- frontend/src/components/Listing.vue
+++ frontend/src/components/Listing.vue
@@ -1,13 +1,19 @@
 <template>
-    <div></div>
+    <div>quantity= {{quantity}}
+    price=10
+    userName="user"
+    item="Item"</div>
 </template>
 
 <script>
-
 export default {
     name: 'Listing',
     props: {
-
+      quantity:Number,
+      price:Number,
+      userName:String,
+      item:String,
+      
     }
 }
 
