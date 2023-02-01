06ddb9b0ec150068baead0e82e5f7f9385debab4
MatinaC553 <118002786+MatinaC553@users.noreply.github.com>
2023-02-01 16:49:01 +0200

    2.1 and 2.2

--- frontend/src/components/DisplayListings.vue
+++ frontend/src/components/DisplayListings.vue
@@ -1,23 +1,30 @@
 <template>
+    <div>
+        <input type="text" v-model="input" placeholder="Search here any item..." />
+        <button type="button" @click="searchBar">Search</button>
+    </div>
     <div class="listings_background">
         <div v-for="(listing, index) in listings" v-bind:key="index">
-            
-            <Listing :item="listing.item.name" :price="listing.price" :quantity="listing.quantity" :username="listing.user.name" />
-            
+            <Listing :item="listing.item.uniqueName" :price="listing.price" :quantity="listing.quantity"
+                :userName="listing.user.name" />
         </div>
     </div>
 </template>
 
 <script>
-import { getAllListings } from '@/services/DataService';
+import { getAllListings, getListingsByName } from '@/services/DataService';
 import Listing from "@/components/Listing.vue";
 
 export default {
     name: 'DisplayListings',
     data: () => {
         return {
-            listings: []
+            listings: [],
+            input: '',
         }
+    },
+    props: {
+        items: Array
     },
     mounted() {
         getAllListings(25).then(data => {
@@ -26,6 +33,21 @@ export default {
     },
     components: {
         Listing
+    },
+    methods: {
+        searchBar: function () {
+            if (this.input.length) {
+                for (let item of this.items) {
+                    if (this.input == item.uniqueName) {
+                        getListingsByName(this.input).then(data => {
+                            this.listings = data;
+                        })
+                        break;
+                    }
+                }
+            }
+
+        }
     }
 }
 </script>
